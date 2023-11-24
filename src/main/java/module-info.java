7842a43d579b3b3470860ae8be54f7281523cc69
Vo Trong Dan <trongdantvh@gmail.com>
2023-11-24 17:13:17 +0700

    optimize gui

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.example.dictionary {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-
+    requires javafx.web;
 
     opens com.example.dictionary to javafx.fxml;
     exports com.example.dictionary;
